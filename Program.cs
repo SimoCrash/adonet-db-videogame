cf3028294301f65fa1804f86aae25beff9cd2cc7
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-30 15:33:24 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+var connStr = "Data Source=localhost;Initial Catalog=db-videogames-query;Integrated Security=True";
+
+using var conn = new SqlConnection(connStr);
+
+try
+{
+    conn.Open();
+}
+catch
+{
+
+}
