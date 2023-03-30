d461e7d40a4b0d86b7887ffd65988a972910029c
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-30 17:33:26 +0200

    problema nella stampa del db

--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
-﻿using System.Data.SqlClient;
+﻿using adonet_db_videogame;
+using System.Data.SqlClient;
 
 var connStr = "Data Source=localhost;Initial Catalog=db-videogames-query;Integrated Security=True";
 
 using var conn = new SqlConnection(connStr);
 
+var videogames = new List<Videogame>();
+
+
 try
 {
     conn.Open();
-}
-catch
-{
+    var query = "SELECT id, name, release_date, software_house_id description FROM videogames";
+    using var command = new SqlCommand(query, conn);
+    using var reader = command.ExecuteReader();
 
+    while (reader.Read())
+    {
+        var id = reader.GetInt64(0);
+        var name = reader.GetString(1);
+        var realease_date = reader.GetDateTime(2);
+        var softwareHouse = reader.GetInt64(3);
+
+        var v = new Videogame(id, name, realease_date, softwareHouse);
+        videogames.Add(v);
+    }
 }
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+foreach (var videogame in videogames) Console.WriteLine(videogame.ToString());
--- Videogame.cs
+++ Videogame.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace adonet_db_videogame
 {
-    internal class Videogame
+    public class Videogame
     {
+        public Videogame(long id, string name, DateTime realease_date, long softwareHouse)
+        {
+            this.id = id;
+            this.name = name;
+            this.realease_date = realease_date;
+            this.softwareHouse = softwareHouse;
+        }
+
+        public long id { get; set; }
+        public string name { get; set; }
+        public DateTime realease_date { get; set; }
+        public long softwareHouse { get; set; }
     }
 }
